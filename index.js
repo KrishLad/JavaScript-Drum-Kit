e71e3732529d6c289ea6d41faec413be08915ff3
Krishna Lad <63975259+KrishLad@users.noreply.github.com>
2022-07-27 11:48:13 -0400

    Adding Starting Files

---
+++ index.js
@@ -0,0 +1,63 @@
+
+//button press
+for (var i = 0; i < document.querySelectorAll(".drum").length;i++ )
+{
+    document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+        makeSound(this.innerHTML);
+        buttonAnimation(this.innerHTML);
+    });
+}
+
+//keyboard press
+document.addEventListener("keydown", function (e) { 
+    
+    makeSound(e.key);
+    buttonAnimation(e.key);
+}); 
+
+
+function makeSound(key) {
+     var audio;
+       switch (key) {
+        case 'w':
+            audio = new Audio("sounds/crash.mp3"); 
+            audio.play(); 
+            break;
+        case 'a':
+            audio = new Audio("sounds/kick-bass.mp3");
+            audio.play(); 
+            break;
+        case 's':
+            audio = new Audio("sounds/snare.mp3");
+            audio.play(); 
+            break;
+        case 'd':
+            audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case 'j': 
+            audio = new Audio("sounds/tom-2.mp3");
+            audio.play(); 
+            break;
+        case 'k':
+            audio = new Audio("sounds/tom-3.mp3");
+            audio.play(); 
+            break;
+        case 'l': 
+            audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+        default:
+            break;
+       }
+    
+}
+
+function buttonAnimation(currentKey) {
+    var buttonCurrent = document.querySelector("."+currentKey);
+    buttonCurrent.classList.add("pressed");
+    
+    setTimeout(function (){
+        buttonCurrent.classList.remove("pressed");
+    },100);
+}
